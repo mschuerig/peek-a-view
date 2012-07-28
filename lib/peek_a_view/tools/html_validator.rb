2c8cd623d9a1a31bc98aa17f58d3bdd8f1be96aa
Michael Schuerig <michael@schuerig.de>
2012-07-28 19:32:31 +0200

    Write HTML validation reports in junit format.

--- lib/peek_a_view/tools/html_validator.rb
+++ lib/peek_a_view/tools/html_validator.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 require 'w3c_validators'
 require 'peek_a_view/tools/checker'
+require 'builder/xmlmarkup'
 
 module PeekAView
   module Tools
@@ -10,9 +11,12 @@ module PeekAView
         raise ArgumentError, "Option validator_uri must be specified" unless validator_uri
         super
 
+        # Somehow the validator does not get this from the response
+        charset = (options[:charset] || 'UTF-8').upcase
+
         @validator = W3CValidators::NuValidator.new(
           validator_uri: validator_uri,
-          charset:       'UTF-8' # somehow the validator does not get this from the response
+          charset:       charset
         )
       end
 
@@ -22,25 +26,48 @@ module PeekAView
 
       def report(uri)
         results = check(uri)
-
-        File.open(report_file(uri), 'w') do |out|
-          out.puts "*** Valid: #{results.is_valid?}"
-          out.puts
-          print_hash(out, '*** Errors', results.errors)
-          out.puts
-          print_hash(out, '*** Warnings', results.warnings)
-          out.puts
-        end
+        write_report(uri, results)
       end
 
       private
+
+      def write_report(uri, results)
+        xml = Builder::XmlMarkup.new(indent: 2)
+        xml.instruct!
+
+        tests    = results.errors.size + results.warnings.size
+        failures = results.errors.size
+
+        xml.testsuite(name: "HTML5 Validation for #{uri}", tests: tests, failures: failures) do
+          results.errors.each do |error|
+            write_testcase(xml, error, 'error')
+          end
+          results.warnings.each do |warning|
+            write_testcase(xml, warning, 'warning')
+          end
+        end
+
+        File.open(report_file(uri), 'w') do |out|
+          out.puts(xml.target!)
+        end
+      end
+
+      def write_testcase(xml, testcase, status)
+        short_message = testcase.message[/^(.{20,}?\.)/, 1]
+        xml.testcase(name: short_message, status: status) do
+          xml.failure(message: testcase.message) do
+            xml.text!("At #{testcase.line}:#{testcase.col}") if testcase.line.present?
+            xml.cdata!(testcase.source) if testcase.source.present?
+          end
+        end
+      end
 
       def report_dir
         File.join(Rails.root, 'reports', 'html_validator')
       end
 
       def report_file(uri)
-        report_path(uri, '.txt')
+        report_path(uri, '.xml')
       end
     end
   end
